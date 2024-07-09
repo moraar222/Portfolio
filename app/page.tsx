c779e8e80188578a48a9629665013cbe8b06739c
Moraar222 <moraar976@gmail.com>
2024-07-09 15:52:36 +0300

    Portfolio updated

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/lib/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      
+    </section>
+  );
+}
