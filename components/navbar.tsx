17e96ba422685208033f2ca76796f683bf3742b9
Moraar222 <moraar976@gmail.com>
2024-07-11 21:56:36 +0300

    updates

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -43,12 +43,12 @@ export default function App() {
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
               <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              <p className="font-semibold">moraar976@gmail.com</p>
             </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="settings">Home</DropdownItem>
+            <DropdownItem key="team_settings">contact</DropdownItem>
+            <DropdownItem key="analytics">About</DropdownItem>
+            <DropdownItem key="system">My projects</DropdownItem>
             <DropdownItem key="configurations">Configurations</DropdownItem>
             <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
             <DropdownItem key="logout" color="danger">
